5f77a798fb3775f6c5051e80dfc2c8e5dd5febb1
christian <christianjtaillon@gmail.com>
2023-02-17 12:36:10 -0700

    corrected 3 bugs

--- splunkuf_install.ps1
+++ splunkuf_install.ps1
@@ -12,15 +12,18 @@
   This version downloads Splunk Universal Forwarder 9.3.0 for Windows.
 
 .NOTES
-  This is not intended for use in a production environment but rather in a lab, learning, or Poc environment.
-  Change the password
+  This is not intended for use in a production environment but rather in a lab, learning, or Poc environment. By default, this script will automatically generate a random password for you. You can modify the script to set the password you desire during installation (view line 22 & 27).
+
 .LINK
     [Information regarding the Splunk Universal Forwarder](https://www.splunk.com/en_us/blog/learn/splunk-universal-forwarder.html)
     [Inputs.conf](https://docs.splunk.com/Documentation/Splunk/9.0.3/Admin/Inputsconf)
  #>
+
+# If you would like to set your own passphrase, modify the following and set it here. Otherwise, by default, the password is randomly generated.
 $passphrase = "Unsold4-8x64b-Monoxide-Sector"
 
 wget -O splunkforwarder-9.0.3-dd0128b1f8cd-x86-release.msi "https://download.splunk.com/products/universalforwarder/releases/9.0.3/windows/splunkforwarder-9.0.3-dd0128b1f8cd-x86-release.msi"
 
-## Comment to Disable Installation
-msiexec /i .\splunkforwarder-9.0.3-dd0128b1f8cd-x86-release.msi AGREETOLICENSE=Yes GENRANDOMPASSWORD=$passphrase
+## If you desire to set a password, uncomment line 28 and comment line 29.
+# msiexec /i splunkforwarder-9.0.3-dd0128b1f8cd-x86-release.msi AGREETOLICENSE=Yes SPLUNKPASSWORD=$passphrase
+msiexec /i splunkforwarder-9.0.3-dd0128b1f8cd-x86-release.msi AGREETOLICENSE=Yes GENRANDOMPASSWORD=1
