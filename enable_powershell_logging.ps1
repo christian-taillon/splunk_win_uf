eed9308f70c86ad24c46bc0cee0692fb8e3f3e53
christian <christiantaillon_public@pm.me>
2023-08-01 12:43:08 -0700

    ps logging

---
+++ enable_powershell_logging.ps1
@@ -0,0 +1,8 @@
+## Powershell Transcription
+Set-ItemProperty -Path 'Software\Policies\Microsoft\Windows\PowerShell\Transcription' -Name EnableTranscripting -Value 1
+
+### NOTES
+## https://woshub.com/how-to-access-and-manage-windows-registry-with-powershell/
+## https://docs.splunk.com/Documentation/UBA/5.2.0/GetDataIn/AddPowerShell
+
+## Transcript Location: c:\users\*\documents\PowerShell_transcript.*.txt
