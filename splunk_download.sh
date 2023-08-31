03c1f742b1cd0f7dd7f7a4a869a7b2bad29c2501
christian <christiantaillon_public@pm.me>
2023-08-30 19:51:44 -0700

    Update splunk version

--- splunk_download.sh
+++ splunk_download.sh
@@ -1,1 +1,1 @@
-wget -O splunk-9.0.4-de405f4a7979-Linux-x86_64.tgz "https://download.splunk.com/products/splunk/releases/9.0.4/linux/splunk-9.0.4-de405f4a7979-Linux-x86_64.tgz"
+wget -O splunk-9.1.1-64e843ea36b1-Linux-x86_64.tgz "https://download.splunk.com/products/splunk/releases/9.1.1/linux/splunk-9.1.1-64e843ea36b1-Linux-x86_64.tgz"
