38e3c221e7ee9337d3986cfebcdddb3ea7f4fc58
christian <christiantaillon_public@pm.me>
2024-01-03 16:39:47 -0700

    Adding nix.

---
+++ splunk_uf_nix.sh
@@ -0,0 +1,1 @@
+wget -O splunkforwarder-9.1.2-b6b9c8185839-Linux-x86_64.tgz "https://download.splunk.com/products/universalforwarder/releases/9.1.2/linux/splunkforwarder-9.1.2-b6b9c8185839-Linux-x86_64.tgz"
